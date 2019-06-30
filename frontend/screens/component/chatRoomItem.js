959c4b0207b8eeafda549f47673c127e26bc2e3a
Alan Yin <>
2019-06-30 13:48:55 +0800

    fix : loginScreen

--- frontend/screens/component/chatRoomItem.js
+++ frontend/screens/component/chatRoomItem.js
@@ -15,23 +15,23 @@ export default class chatRoomItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          messages: [],
-          username:''
+            messages: [],
+            username: ''
         };
         AsyncStorage.getItem('name').then((name) => {
-          console.log(name)
-          this.setState({username:name})
-       })
-      }
-    
+            console.log(name)
+            this.setState({ username: name })
+        })
+    }
+
 
 
 
     render() {
-        const opponent = this.props.data.names.map( item => {
+        const opponent = this.props.data.names.map(item => {
             console.log(item)
             console.log(this.state.username)
-            if(item === this.state.username){
+            if (item === this.state.username) {
                 return;
             }
             else return item
@@ -39,12 +39,23 @@ export default class chatRoomItem extends React.Component {
         console.log(opponent)
         return (
             <TouchableOpacity
-                style={{ flex: 1, flexDirection: 'row', backgroundColor: 'grey', height: 100, alignItems: 'stretch' }}
-                onPress={() => { this.props.navigation.push('ChatRoomDetail',{
-                        data : this.props.data
-                }) }}
+                style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 1, borderColor: '#ff9d00', height: 100, alignItems: 'stretch' }}
+                onPress={() => {
+                    this.props.navigation.push('ChatRoomDetail', {
+                        data: this.props.data
+                    })
+                }}
             >
-            <Text>{opponent}</Text>
+                <View style={{ flex: 1, flexDirection:'row',alignItems: 'center', justifyContent: 'flex-start',paddingLeft:30 }}>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'grey' }}></View>
+
+                    <Text style={{
+                        marginLeft:50,
+                        fontSize: 30,
+
+                    }}>{opponent}</Text>
+                </View>
+
             </TouchableOpacity>
         );
     }
